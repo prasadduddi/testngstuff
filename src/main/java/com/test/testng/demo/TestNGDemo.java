04646a0506f6dc215c8fc1f71c3494d18174b602
DESKTOP-KF0OD3I\PRASAD <pduddi@sapient.com>
2018-09-06 22:07:40 +0100

    first commit

---
+++ src/main/java/com/test/testng/demo/TestNGDemo.java
@@ -0,0 +1,32 @@
+package com.test.testng.demo;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+
+public class TestNGDemo {
+    public String baseUrl = "http://www.google.com/xhtml";
+    String driverPath = "C:\\Dev\\SeleniumDrivers\\chromedriver.exe";
+    public WebDriver driver ; 
+     
+  @Test
+  public void verifyHomepageTitle() throws InterruptedException {
+       
+      System.out.println("launching chrome browser"); 
+     // System.setProperty("webdriver.firefox.marionette", driverPath);
+      
+      System.setProperty("webdriver.chrome.driver", driverPath);
+      
+      WebDriver driver = new ChromeDriver();
+      driver.get(baseUrl);
+      Thread.sleep(5000);  // Let the user actually see something!
+      WebElement searchBox = driver.findElement(By.name("q"));
+      searchBox.sendKeys("ChromeDriver");
+      searchBox.submit();
+      Thread.sleep(5000);  // Let the user actually see something!
+      driver.quit();
+  }
+}
